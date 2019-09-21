76f9d81cacd583055bc99fe678578741cecbd81c
zhangboyu1 <48872652+zhangboyu1@users.noreply.github.com>
2019-09-21 22:38:30 +1000

    Update App.js

--- App.js
+++ App.js
@@ -3,56 +3,44 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        div className = "wrapper" >
-        <
-        div className = "header" >
-        <
-        div header_left >
-        123 <
-        /div> <
-        div header_right >
-        456 <
-        /div> <
-        /div> <
-        button class = "btn" > 123 < /button> <
-        div className = "cardbody" >
-        <
-        div className = "line1" >
-        <
-        div className = "line1_left" >
-        <
-        div className = "line1_left_top" > < /div> <
-        div className = "line1_left_mid11" > < /div> <
-        div className = "line1_left_bottom" >
-        <
-        div className = "line1_left_bottom-humi" > < /div> <
-        div className = "line1_left_bottom-wind" > < /div> <
-        /div> <
-        /div> <
-        div className = "line1_right" > < /div>
-
-        <
-        /div> <
-        div className = "line2" >
-        <
-        div className = "line2_left" >
-        <
-        div className = "line2_left_top" > < /div> <
-        div className = "line2_left_mid" > < /div> <
-        div className = "line2_left_bottom" > < /div> <
-        /div> <
-        div className = "line2_right" > < /div> <
-        /div> <
-        div className = "line3" > < /div> <
-        /div>
-
-
-        <
-        /div> <
-        /div>
+    return (
+        <div className="App">
+        <div className="wrapper">
+            <div className="header">
+                <div header_left>123 </div> 
+                <div header_right>456 </div> 
+            </div> 
+            <button class="btn"> 123 </button> 
+            <div className="cardbody">
+                                
+                <div className="line1">
+                                    
+                    <div className="line1_left">
+                                        
+                        <div className="line1_left_top"></div> 
+                        <div className="line1_left_mid11"></div> 
+                        <div className="line1_left_bottom">
+                                                    
+                            <div className="line1_left_bottom-humi"></div> 
+                            <div className="line1_left_bottom-wind"></div> 
+                        </div> 
+                    </div> 
+                    <div className="line1_right"></div> 
+                </div> 
+                <div className="line2">
+                                                                    
+                    <div className="line2_left">
+                                
+                        <div className="line2_left_top"></div> 
+                        <div className="line2_left_mid"> </div> 
+                        <div className="line2_left_bottom"></div> 
+                    </div> 
+                    <div className="line2_right"></div> 
+                </div> 
+                <div className="line3"></div> 
+            </div>
+         </div> 
+        </div>
     );
 }
 
